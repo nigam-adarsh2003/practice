b64471c4cd7d14a8ab0a6c34a945d908db66c1e8
nigam-adarsh2003 <119562732+nigam-adarsh2003@users.noreply.github.com>
2023-04-11 22:54:03 +0530

    Add files via upload

---
+++ valid_parentheses.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for ch in s:
+            if ch == '(' or ch == '{' or ch == '[':
+                stack.append(ch)
+            else:
+                if not stack:
+                    return False
+                if ch == ')' and stack[-1] == '(':
+                    stack.pop()
+                elif ch == '}' and stack[-1] == '{':
+                    stack.pop()
+                elif ch == ']' and stack[-1] == '[':
+                    stack.pop()
+                else:
+                    return False
+        return not stack
